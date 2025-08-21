199ae8099ea78f2294744247b9de39ee1ec62f62
chrysellia <priscilliachryso@gmail.com>
2025-08-21 13:58:55 +0300

    back : update controller

---
+++ src/Controller/Api/TicketApiController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+#[Route('/api/tickets')]
+class TicketApiController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private TaskRepository $taskRepository,
+        private SerializerInterface $serializer,
+        private ValidatorInterface $validator
+    ) {}
+
+    #[Route('', name: 'api_tickets_list', methods: ['GET'])]
+    public function list(): JsonResponse
+    {
+        $tasks = $this->taskRepository->findAll();
+        return $this->json($tasks, Response::HTTP_OK, [], ['groups' => ['task:read']]);
+    }
+
+    #[Route('', name: 'api_tickets_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        $task = $this->serializer->deserialize(
+            $request->getContent(),
+            Task::class,
+            'json',
+            ['groups' => ['task:write']]
+        );
+
+        if (!$task->getStatus()) {
+            $task->setStatus(Task::STATUS_TODO);
+        }
+
+        $errors = $this->validator->validate($task);
+        if (count($errors) > 0) {
+            return $this->json(['errors' => (string) $errors], Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->entityManager->persist($task);
+        $this->entityManager->flush();
+
+        return $this->json($task, Response::HTTP_CREATED, [], ['groups' => ['task:read']]);
+    }
+
+    #[Route('/{id}', name: 'api_tickets_update', methods: ['PUT'])]
+    public function update(Task $task, Request $request): JsonResponse
+    {
+        $this->serializer->deserialize(
+            $request->getContent(),
+            Task::class,
+            'json',
+            [
+                'groups' => ['task:write'],
+                'object_to_populate' => $task,
+            ]
+        );
+
+        $errors = $this->validator->validate($task);
+        if (count($errors) > 0) {
+            return $this->json(['errors' => (string) $errors], Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->entityManager->flush();
+
+        return $this->json($task, Response::HTTP_OK, [], ['groups' => ['task:read']]);
+    }
+
+    #[Route('/{id}', name: 'api_tickets_delete', methods: ['DELETE'])]
+    public function delete(Task $task): JsonResponse
+    {
+        $this->entityManager->remove($task);
+        $this->entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,13 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class DefaultController extends AbstractController
+class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->redirectToRoute('app_ticket_board');
+    }
+    
+    #[Route('/board', name: 'app_ticket_board')]
+    public function ticketBoard(): Response
+    {
+        return $this->render('ticket/board.html.twig');
     }
 }
